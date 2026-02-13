d86e5cfc6539dc0a637f2f89e0f02ef2edd96011
Artsiom Bukhautsou <artsiom.bukhautsou@gmail.com>
2026-02-13 08:50:16 +0100

    backporting: Per rack clenup annotation

--- pkg/reconciliation/reconcile_racks.go
+++ pkg/reconciliation/reconcile_racks.go
@@ -21,6 +21,7 @@ import (
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
@@ -2416,7 +2417,7 @@ func (rc *ReconciliationContext) cleanupAfterScaling() result.ReconcileResult {
 		}
 
 		// Create the cleanup task
-		if err := rc.createTask(taskapi.CommandCleanup); err != nil {
+		if err := rc.createCleanupTask(); err != nil {
 			return result.Error(err)
 		}
 
@@ -2428,9 +2429,14 @@ func (rc *ReconciliationContext) cleanupAfterScaling() result.ReconcileResult {
 	return result.Continue()
 }
 
-func (rc *ReconciliationContext) createTask(command taskapi.CassandraCommand) error {
-	generatedName := fmt.Sprintf("%s-%d", command, time.Now().Unix())
+func (rc *ReconciliationContext) createCleanupTask() error {
+	generatedName := fmt.Sprintf("%s-%d", taskapi.CommandCleanup, time.Now().Unix())
 	dc := rc.Datacenter
+
+	var maxConcurrentPods *int
+	if metav1.HasAnnotation(rc.Datacenter.ObjectMeta, api.EnableParallelCleanupWithinRackAnnotation) {
+		maxConcurrentPods = rc.calculateMaxConcurrentPods()
+	}
 
 	task := &taskapi.CassandraTask{
 		ObjectMeta: metav1.ObjectMeta{
@@ -2446,10 +2452,11 @@ func (rc *ReconciliationContext) createTask(command taskapi.CassandraCommand) er
 			CassandraTaskTemplate: taskapi.CassandraTaskTemplate{
 				Jobs: []taskapi.CassandraJob{
 					{
-						Name:    fmt.Sprintf("%s-%s", command, rc.Datacenter.Name),
-						Command: command,
+						Name:    fmt.Sprintf("%s-%s", taskapi.CommandCleanup, rc.Datacenter.Name),
+						Command: taskapi.CommandCleanup,
 					},
 				},
+				MaxConcurrentPods: maxConcurrentPods,
 			},
 		},
 		Status: taskapi.CassandraTaskStatus{},
@@ -2470,6 +2477,10 @@ func (rc *ReconciliationContext) createTask(command taskapi.CassandraCommand) er
 	rc.Datacenter.Status.AddTaskToTrack(task.ObjectMeta)
 
 	return rc.Client.Status().Patch(rc.Ctx, dc, dcPatch)
+}
+
+func (rc *ReconciliationContext) calculateMaxConcurrentPods() *int {
+	return ptr.To(rc.desiredRackInformation[0].NodeCount)
 }
 
 func (rc *ReconciliationContext) activeTaskCompleted(task *taskapi.CassandraTask) result.ReconcileResult {
--- tests/scale_up/scale_up_suite_test.go
+++ tests/scale_up/scale_up_suite_test.go
@@ -9,7 +9,6 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
 	corev1 "k8s.io/api/core/v1"
 
 	"github.com/k8ssandra/cass-operator/tests/kustomize"
@@ -75,7 +74,7 @@ var _ = Describe(testName, func() {
 			ns.WaitForDatacenterCondition(dcName, "ScalingUp", string(corev1.ConditionFalse))
 
 			// Ensure that when 'ScaleUp' becomes 'false' that our pods are in fact up and running
-			Expect(len(ns.GetDatacenterReadyPodNames(dcName))).To(Equal(2))
+			Expect(ns.GetDatacenterReadyPodNames(dcName)).To(HaveLen(2))
 
 			ns.WaitForDatacenterReady(dcName)
 
@@ -111,10 +110,30 @@ var _ = Describe(testName, func() {
 			// Also verify that we have exactly 3 tasks, no more
 			ns.CheckForCassandraTasks(dcName, "cleanup", false, 3)
 
+			step = "add annotation to enable parallel cleanup within rack"
+			json = "{\"metadata\": {\"annotations\": {\"cassandra.datastax.com/enable-parallel-cleanup-within-rack\": \"true\"}}}"
+			k = kubectl.PatchMerge(dcResource, json)
+			ns.ExecAndLog(step, k)
+			ns.WaitForDatacenterReady(dcName)
+
+			step = "scale up to 6 nodes"
+			json = "{\"spec\": {\"size\": 6}}"
+			k = kubectl.PatchMerge(dcResource, json)
+			ns.ExecAndLog(step, k)
+			ns.WaitForDatacenterReady(dcName)
+
+			step = "checking that cassandratask has expected number of maxConcurrentPods"
+			json = "jsonpath={.items[?(@.spec.maxConcurrentPods)].spec.maxConcurrentPods}"
+			k = kubectl.Get("cassandratask").
+				WithLabel(fmt.Sprintf("cassandra.datastax.com/datacenter=%s", dcName)).
+				FormatOutput(json)
+			ns.WaitForOutputAndLog(step, k, "6", 20)
+			ns.WaitForCompletedCassandraTasks(dcName, "cleanup", 4)
+
 			step = "check recorded host IDs"
 			ns.Log(step)
 			nodeStatusesHostIds := ns.GetNodeStatusesHostIds(dcName)
-			Expect(len(nodeStatusesHostIds), 5)
+			Expect(nodeStatusesHostIds).To(HaveLen(6))
 
 			step = "deleting the dc"
 			k = kubectl.DeleteFromFiles(testFile)
